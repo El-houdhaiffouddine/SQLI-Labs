7982b7c05883d9b8d83c16d9fff7980a79eef952
Ben Sidi EL-HOUDHAIFFOUDDINE <bensidi.elhoudhaiffouddine@esprit.tn>
2025-05-14 10:20:50 +0000

    Updating file app.py and adding new HTML file redirect.html

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/user1/employees.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/employees.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
